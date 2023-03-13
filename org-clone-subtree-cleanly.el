68af8e017264b13c4d595afd507ed6aa36d74192
Hauke Rehfeld <git@haukerehfeld.de>
2023-03-13 21:30:28 +0100

    add: initial

---
+++ org-clone-subtree-cleanly.el
@@ -0,0 +1,21 @@
+;;; org-clone-subtree-cleanly.el --- Clone orgmode subtrees without duplicating IDs
+;;; Author: Hauke Rehfeld <emacs@haukerehfeld.de>
+;;; Version: 0.1
+;;; Package-Requires: ((emacs "24.1") org)
+;;; Keywords: convenience
+;;; URL: https://github.com/hrehfeld/org-clone-subtree-cleanly
+;;; Commentary:
+
+;;; Code:
+(require 'org)
+
+(defun org-clone-subtree-cleanly ()
+  "Clone a subtree, but delete all ID properties to avoid duplicate IDs."
+  (interactive)
+  (org-copy-subtree)
+  (org-end-of-subtree)
+  (org-paste-subtree)
+  (org-map-entries (lambda () (org-delete-property "ID")) nil 'tree))
+
+(provide 'org-clone-subtree-cleanly)
+;;; org-clone-subtree-cleanly.el ends here
